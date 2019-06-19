a8a87e623293fa7e92adcab31a60b0977c79104c
WangShaoBin <wangshaobin@arrail-dental.com>
2019-06-19 15:21:15 +0800

    提交Controller

---
+++ src/main/java/com/app/wangshaobin/DemoController.java
@@ -0,0 +1,12 @@
+package com.app.wangshaobin;
+
+/**
+ * @Package com.app.wangshaobin
+ * @ClassName DemoController
+ * @Author shaobin.wang
+ * @Date 2019/06/19 15:20
+ * @Version 1.0
+ * @Description:
+ **/
+public class DemoController {
+}
