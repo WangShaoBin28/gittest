c44a638607128662617fdbfe64ae38d76f5e4191
WangShaoBin <wangshaobin@arrail-dental.com>
2019-06-19 15:20:09 +0800

    Revert "版本1"
    This reverts commit 0a2017b

--- src/main/java/com/app/GittestApplication.java
+++ src/main/java/com/app/GittestApplication.java
@@ -6,13 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class GittestApplication {
 
-    /**
-     * @param null
-     * @return
-     * @author shaobin.wang
-     * @date 2019/06/19 15:17
-     * @description 测试版本回滚
-     */
     public static void main(String[] args) {
         SpringApplication.run(GittestApplication.class, args);
     }
